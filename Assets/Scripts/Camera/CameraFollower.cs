5f802b499b253ea7a68316ccf75a6fb2e5a47b84
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 17:05:12 +0300

    CameraFollower imp

---
+++ Assets/Scripts/Camera/CameraFollower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+    public float rotationSpeed;
+    [Header("Offsets")]
+    public Vector3 offset;
+    public Vector3 finishOffset;
+    [Header("Rotations")]
+    public Vector3 rotation;
+    public Vector3 finishRotation;
+
+    void Update()
+    {
+        if(target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, speed * Time.deltaTime);
+
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
