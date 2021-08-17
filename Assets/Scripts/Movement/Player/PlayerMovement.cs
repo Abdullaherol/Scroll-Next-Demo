f60b297df62ede146d6840fbd26e41528bb6fe7d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 16:43:36 +0300

    PlayerMovement improvement

--- Assets/Scripts/Movement/Player/PlayerMovement.cs
+++ Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -18,34 +18,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (groundChecker.OnGround)
         {
-            targetPosition = transform.position + transform.forward * maxDistance;
+            targetPosition = transform.position;
 
             Vector2 swipe = swipeController.Swipe * swipeSpeed;
 
             switch (direction)
             {
                 case MovementDirection.X:
-                    targetPosition += new Vector3(swipe.x, 0, 0);
-                    
-                    break;
-                case MovementDirection.Y:
-                    targetPosition += new Vector3(0, swipe.x,0);
+                    targetPosition += new Vector3(maxDistance, 0, swipe.x);
                     break;
                 case MovementDirection.Z:
-                    targetPosition += new Vector3(0, 0, swipe.x);
+                    targetPosition += new Vector3(swipe.x, 0, maxDistance);
                     break;
                 default:
                     break;
             }
-
-            transform.position = Vector3.Lerp(targetPosition, targetPosition, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            transform.LookAt(targetPosition);
         }
     }
 
     public enum MovementDirection
     {
         X,
-        Y,
         Z
     }
 }
