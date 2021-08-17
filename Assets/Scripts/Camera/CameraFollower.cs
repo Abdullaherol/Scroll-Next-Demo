7c0a90ff6221cb7caf36019521d47ca573948551
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 17:11:36 +0300

    GameManager imp and CameraFollower variable dec

--- Assets/Scripts/Camera/CameraFollower.cs
+++ Assets/Scripts/Camera/CameraFollower.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
+    public GameManager gameManager;
     public GameObject target;
+
     public float speed;
     public float rotationSpeed;
+    
     [Header("Offsets")]
     public Vector3 offset;
     public Vector3 finishOffset;
+    
     [Header("Rotations")]
     public Vector3 rotation;
     public Vector3 finishRotation;
@@ -18,9 +22,19 @@ public class CameraFollower : MonoBehaviour
     {
         if(target != null)
         {
-            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, speed * Time.deltaTime);
+            if (!gameManager.gameFinished)
+            {
+                transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, speed * Time.deltaTime);
 
-            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotation, rotationSpeed * Time.deltaTime);
+                transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotation, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, target.transform.position + finishOffset, speed * Time.deltaTime);
+
+                transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, finishRotation, rotationSpeed * Time.deltaTime);
+            }
+            
         }
     }
 }
