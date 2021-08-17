b2c8025944c852b9e273c5c2ce00590a1889ce3a
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 15:28:50 +0300

    Ground and GroundManager imp

---
+++ Assets/Scripts/Environment/Ground/Ground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private GroundManager groundManager;
+    private Animator animator;
+    private Rigidbody rgb;
+
+    public void FallStart(GroundManager groundManager)
+    {
+        animator = GetComponent<Animator>();
+        rgb = gameObject.AddComponent<Rigidbody>();
+
+        animator.Play("Fall");
+        rgb.useGravity = true;
+    }
+
+    void Update()
+    {
+        if(transform.position.y < groundManager.limitY)
+        {
+            groundManager.TeleportPool(gameObject);
+        }
+    }
+}
