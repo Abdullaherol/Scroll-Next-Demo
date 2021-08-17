61efc36af9207143b1ac193e5f783e4a538cc132
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 15:44:44 +0300

    PlayerMovement and SwipeController improvement

--- Assets/Scripts/Movement/Player/PlayerMovement.cs
+++ Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
+    public float speed;
+    public float maxDistance;
+    public float swipeSpeed;
+    public MovementDirection direction;
 
-    }
+    public SwipeController swipeController;
+    public PlayerGroundChecker groundChecker;
+
+    private Vector3 targetPosition;
 
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.up * -1, out hit))
+        if (groundChecker.OnGround)
         {
+            targetPosition = transform.position + transform.forward * maxDistance;
 
+            Vector2 swipe = swipeController.Swipe * swipeSpeed;
+
+            switch (direction)
+            {
+                case MovementDirection.X:
+                    targetPosition += new Vector3(0, 0, swipe.x);
+                    break;
+                case MovementDirection.Y:
+                    targetPosition += new Vector3(0, swipe.x,0);
+                    break;
+                case MovementDirection.Z:
+                    targetPosition += new Vector3(swipe.x, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+
+            transform.position = Vector3.Lerp(targetPosition, targetPosition, speed * Time.deltaTime);
         }
+    }
+
+    public enum MovementDirection
+    {
+        X,
+        Y,
+        Z
     }
 }
